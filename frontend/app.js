6f07d3d810392f46b9d107ec521bd780c1fc5616
Kartik Gahlot <kartikgahlot0865@gmail.com>
2026-06-14 18:19:02 +0530

    Updated

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = 'https://book-recommendation-system-1rgv.onrender.com';
 
 const searchInput = document.getElementById('searchInput');
 const searchButton = document.getElementById('searchButton');
